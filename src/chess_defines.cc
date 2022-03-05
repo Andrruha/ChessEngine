ad5d0d71c757d8c13e13fa3da49f89a909d5197a
Andrey <notAndreyFedorov@yandex.ru>
2022-03-05 16:30:01 +0300

    Add some helper functions

--- src/chess_defines.cc
+++ src/chess_defines.cc
@@ -5,6 +5,72 @@
 
 namespace chess_engine {
 
+Player Opponent(Player player) {
+  if (player == Player::kWhite) {
+    return Player::kBlack;
+  } else if (player == Player::kBlack) {
+    return Player::kWhite;
+  }
+  assert(false);  // invalid player
+  return Player::kNone;
+}
+
+bool operator==(Piece first, Piece second) {
+  return first.type == second.type && first.player == second.player;
+}
+
+bool operator!=(Piece first, Piece second) {
+  return !(first == second);
+}
+
+bool operator==(Coordinates first, Coordinates second) {
+  return first.file == second.file && first.rank == second.rank;
+}
+
+bool operator!=(Coordinates first, Coordinates second) {
+  return !(first == second);
+}
+
+bool BelongsToLine(Segment line, Coordinates point) {
+  Coordinates big_delta;
+  big_delta.file = segment.end.file - segment.start.file;
+  big_delta.rank = segment.end.rank - segment.start.rank;
+  Coordinates small_delta;
+  small_delta.file = point.file - segment.start.file;
+  small_delta.rank = point.rank - segment.start.rank;
+  return small_delta.file*big_delta.rank == small_delta.rank*big_delta.file;
+}
+
+bool BelongsToSegment(Segment segment, Coordinates point) {
+  Coordinates big_delta;
+  big_delta.file = segment.end.file - segment.start.file;
+  big_delta.rank = segment.end.rank - segment.start.rank;
+  Coordinates small_delta;
+  small_delta.file = point.file - segment.start.file;
+  small_delta.rank = point.rank - segment.start.rank;
+  Coordinates raminder;
+  remainder.file = segment.end.file - point.file;
+  remainder.rank = segment.end.rank - point.rank;
+  if (small_delta.file*big_delta.rank != small_delta.rank*big_delta.file) {
+    return false;
+  }
+  if (small_delta.file*remainder.file < 0) {
+    return false;
+  }
+  if (small_delta.rank*remainder.rank < 0) {
+    return false;
+  }
+  return true;
+}
+
+bool operator==(Move first, Move second) {
+  return first.from == second.from && first.to == second.to && first.piece == second.piece;
+}
+
+bool operator!=(Move first, Move second) {
+  return !(first == second);
+}
+
 int8_t DoubleJumpRank(Player player) {
   if (player == Player::kWhite) {
     return 1;
--- src/chess_defines.h
+++ src/chess_defines.h
@@ -4,6 +4,7 @@
 // Contains some basic chess structs and enums to be used by the engine.
 // All code that works with the engine needs most of it.
 
+#include <array>
 #include <cstdint>
 
 namespace chess_engine {
@@ -24,34 +25,42 @@ enum struct Player {
   kBlack = 2
 };
 
+Player Opponent(Player player);
 struct Piece {
   PieceType type;
   Player player;
 };
 
+bool operator==(Piece first, Piece second);
+bool operator!=(Piece first, Piece second);
 namespace pieces {
-  constexpr Piece kNone = {PieceType::kNone, Player::kNone};
-  constexpr Piece kPawn = {PieceType::kPawn, Player::kNone};
-  constexpr Piece kRook = {PieceType::kRook, Player::kNone};
-  constexpr Piece kKnight = {PieceType::kKnight, Player::kNone};
-  constexpr Piece kBishop = {PieceType::kBishop, Player::kNone};
-  constexpr Piece kQueen = {PieceType::kQueen, Player::kNone};
-  constexpr Piece kKing = {PieceType::kKing, Player::kNone};
-  constexpr Piece kWhite = {PieceType::kNone, Player::kWhite};
-  constexpr Piece kWhitePawn = {PieceType::kPawn, Player::kWhite};
-  constexpr Piece kWhiteRook = {PieceType::kRook, Player::kWhite};
-  constexpr Piece kWhiteKnight = {PieceType::kKnight, Player::kWhite};
-  constexpr Piece kWhiteBishop = {PieceType::kBishop, Player::kWhite};
-  constexpr Piece kWhiteQueen = {PieceType::kQueen, Player::kWhite};
-  constexpr Piece kWhiteKing = {PieceType::kKing, Player::kWhite};
-  constexpr Piece kBlack = {PieceType::kNone, Player::kBlack};
-  constexpr Piece kBlackPawn = {PieceType::kPawn, Player::kBlack};
-  constexpr Piece kBlackRook = {PieceType::kRook, Player::kBlack};
-  constexpr Piece kBlackKnight = {PieceType::kKnight, Player::kBlack};
-  constexpr Piece kBlackBishop = {PieceType::kBishop, Player::kBlack};
-  constexpr Piece kBlackQueen = {PieceType::kQueen, Player::kBlack};
-  constexpr Piece kBlackKing = {PieceType::kKing, Player::kBlack};
+  Piece kNone = {PieceType::kNone, Player::kNone};
+  Piece kPawn = {PieceType::kPawn, Player::kNone};
+  Piece kRook = {PieceType::kRook, Player::kNone};
+  Piece kKnight = {PieceType::kKnight, Player::kNone};
+  Piece kBishop = {PieceType::kBishop, Player::kNone};
+  Piece kQueen = {PieceType::kQueen, Player::kNone};
+  Piece kKing = {PieceType::kKing, Player::kNone};
+  Piece kWhite = {PieceType::kNone, Player::kWhite};
+  Piece kWhitePawn = {PieceType::kPawn, Player::kWhite};
+  Piece kWhiteRook = {PieceType::kRook, Player::kWhite};
+  Piece kWhiteKnight = {PieceType::kKnight, Player::kWhite};
+  Piece kWhiteBishop = {PieceType::kBishop, Player::kWhite};
+  Piece kWhiteQueen = {PieceType::kQueen, Player::kWhite};
+  Piece kWhiteKing = {PieceType::kKing, Player::kWhite};
+  Piece kBlack = {PieceType::kNone, Player::kBlack};
+  Piece kBlackPawn = {PieceType::kPawn, Player::kBlack};
+  Piece kBlackRook = {PieceType::kRook, Player::kBlack};
+  Piece kBlackKnight = {PieceType::kKnight, Player::kBlack};
+  Piece kBlackBishop = {PieceType::kBishop, Player::kBlack};
+  Piece kBlackQueen = {PieceType::kQueen, Player::kBlack};
+  Piece kBlackKing = {PieceType::kKing, Player::kBlack};
 }
+
+std::array<PieceType,4> kPromotionOptions = {
+  PieceType::kRook, PieceType::kKnight,
+  PieceType::kBishop, PieceType::kQueen
+};
 
 enum struct Castle {
   kKingside = 0,
@@ -63,11 +72,25 @@ struct Coordinates {
   int8_t rank;  // aka "y-coordinate" (0 corresponds to the first rank)
 };
 
+bool operator==(Coordinates first, Coordinates second);
+bool operator!=(Coordinates first, Coordinates second);
+
+sturct Segment {
+  Coordinates start;
+  Coordinates end;
+}
+
+bool BelongsToLine(Segment line, Coordinates point);
+bool BelongsToSegment(Segment segmant, Coordinates point);
+
 struct Move {
   Coordinates from; 
   Coordinates to;
   Piece piece;  // Piece that moves or piece to promote to.
 };
+
+bool operator==(Move first, Move second);
+bool operator!=(Move first, Move second);
 
 int8_t DoubleJumpRank(Player player);
 int8_t PromotionRank(Player player);
