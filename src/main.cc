57c5f13684ea7b4a966f55ccc5b15b6c38d5d8f9
Andrey <notAndreyFedorov@yandex.ru>
2022-04-03 16:05:28 +0300

    Make main function adhere to XBoard protocol

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,18 @@
-#include <iostream>
+#include "chess_defines.h"
+#include "fen.h"
+#include "zobrist_hash.h"
+#include "engine.h"
+#include "engine_manager.h"
+#include "winboard_protocol.h"
 
 int main() {
-  std::cout << "Hello! This is my own chess engine!\n";
+  chess_engine::ZobristHashFunction func(14159265358979323846ull);
+  chess_engine::Position starting_position = chess_engine::FenToPosition (
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+  );
+  chess_engine::Engine engine(starting_position, func);
+  chess_engine::WinboardProtocol protocol;
+  chess_engine::EngineManager manager(&protocol, &engine);
+
+  manager.StartMainLoop();
 }
