7c2bec2ad5d84795cad397678c9fe84ee8cff808
Andrey <notAndreyFedorov@yandex.ru>
2022-08-15 08:51:21 +0300

    Add comments

--- src/main.cc
+++ src/main.cc
@@ -6,10 +6,15 @@
 #include "src/winboard_protocol.h"
 
 int main() {
+  // Create hash-function for chess positions
   chess_engine::ZobristHashFunction func(14159265358979323846ull);
+
+  // Set up the starting position
   chess_engine::Position starting_position = chess_engine::FenToPosition(
     "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
   );
+
+  // Create instances of the engine and protocol
   chess_engine::Engine engine(starting_position, func);
   chess_engine::WinboardProtocol protocol;
   chess_engine::EngineManager manager(&protocol, &engine);
